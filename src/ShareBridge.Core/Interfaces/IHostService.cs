8b73ec4b0efd97974f019d4dd13dddd2287a2c8b
bahodirovich <abduazimerkinov2001@gmail.com>
2022-10-25 01:28:06 +0500

    Create HostService

---
+++ src/ShareBridge.Core/Interfaces/IHostService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareBridge.Core.Interfaces
+{
+    public interface IHostService
+    {
+        Task<string> GetHostNameAsync();
+    }
+}
