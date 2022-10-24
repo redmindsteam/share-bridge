3ea739c4ab5a2b5bbbbc07b98ecbd16e0dd96f77
shahzodochilov <Shahzodxonochilov@gmail.com>
2022-10-25 01:28:52 +0500

    I added Main Page

---
+++ src/ShareBridge.Console/Pages/ServerPage/ServerPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareBridge.Console.Pages.ServerPage
+{
+    public class ServerPage
+    {
+        public static async Task RunAsync()
+        {
+            System.Console.WriteLine("Server started!");
+            string Ip = "192.168.1.1";
+            System.Console.WriteLine($"Your IP address {Ip}");
+        }
+    }
+}
